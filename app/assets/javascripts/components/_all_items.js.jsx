05c89fae741c798def01cef055f9278af2ae8799
OktoberComstock <oktober.comstock@gmail.com>
2017-10-30 20:32:40 -0700

    added new items

--- app/assets/javascripts/components/_all_items.js.jsx
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -1,12 +1,22 @@
 var AllItems = React.createClass({
-  componentDidMount() {
-    console.log('Component mounted')
+  getInitialState() {
+    return { items: [] }
   },
-
+  componentDidMount() {
+    $.getJSON('api/v1/items.json', (response) => { this.setState({ items: response}) });
+  },
   render() {
-    return (
+    var items= this.state.items.map((item) => {
+      return (
+        <div key={item.id}>
+          <h3>{item.name}</h3>
+          <p>{item.description}</p>
+        </div>
+      )
+    });
+    return(
       <div>
-        <h1>All items component</h1>
+        {items}
       </div>
     )
   }
